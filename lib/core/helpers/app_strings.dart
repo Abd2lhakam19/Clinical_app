3773a6070618eea04a05876364efc7e7cc721daa
Abd2lhakam19 <midoahme93@gmail.com>
2023-12-04 11:22:58 +0200

    add [ app_assets , app_strings]

---
+++ lib/core/helpers/app_strings.dart
@@ -0,0 +1,7 @@
+class AppStrings {
+  static const String docDoc = "DocDoc",
+      doctorAppointment = " Best Doctor Appointment App",
+      appDescription =
+          "Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.",
+      getStarted = "Get Started";
+}
--- lib/features/on_boarding/widgets/doc_logo.dart
+++ lib/features/on_boarding/widgets/doc_logo.dart
@@ -1,3 +1,5 @@
+import 'package:clinical_app/core/helpers/app_assets.dart';
+import 'package:clinical_app/core/helpers/app_strings.dart';
 import 'package:clinical_app/core/theming/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,12 +13,12 @@ class DocLogo extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        SvgPicture.asset('assets/svgs/doc_logo.svg'),
+        SvgPicture.asset(AppAssets.mainLogo),
         SizedBox(
           width: 10.w,
         ),
         Text(
-          "Docdoc",
+          AppStrings.docDoc,
           style: TextStyles.font24BlackBold,
         )
       ],
